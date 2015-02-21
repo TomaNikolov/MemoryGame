ccb7f1f92aa42a59b0c31c8ef961c27b93e1b846
TomaNikolov <on.tomas@gmail.com>
2015-02-21 18:34:11 +0200

    //

--- MemoryGames/MemoryGames/ConsoleInput.cs
+++ MemoryGames/MemoryGames/ConsoleInput.cs
@@ -4,13 +4,6 @@ namespace MemoryGames
 {
   public  class ConsoleInput
     {
-        public static CardPosition GetPositionFromInput()
-        {
-            Console.Write("Row: ");
-             int row = int.Parse(Console.ReadLine());
-             Console.Write("Col: ");
-             int col = int.Parse(Console.ReadLine());
-             return new CardPosition(row, col);
-        }
+       
     }
 }
