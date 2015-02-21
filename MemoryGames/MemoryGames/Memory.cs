40eaba495d4ac1b68cb3a36033fa098bf07a2873
TomaNikolov <on.tomas@gmail.com>
2015-02-21 17:33:08 +0200

    windows size is increased

--- MemoryGames/MemoryGames/Memory.cs
+++ MemoryGames/MemoryGames/Memory.cs
@@ -7,7 +7,7 @@ namespace MemoryGames
         public static void Main(string[] args)
         {
            // Console.SetBufferSize(78, 40);
-            Console.SetWindowSize(78, 40);
+            Console.SetWindowSize(78, 50);
             GameBackground.Logo();
            GameManager.Menu();
         }
